a054ae4aabd268657a641e6488d00fd24f0a9001
putrasatr <satriaputrapratama2002@gmail.com>
2022-02-03 14:41:01 +0700

    Perfect

--- index.ts
+++ index.ts
@@ -4,41 +4,40 @@ type AddFunc = (event: ScientCalc) => number;
 type NumParamProps = number[];
 
 interface CalcProps {
-  x?: number;
+  x: number | 0;
 }
 interface PowProps {
-  n: number;
-  powNum: number;
-  countPreviousNumber: BasicCountType;
+  n?: number | 0;
+  x: number;
 }
 class Calculator {
   x: number;
   constructor(x: number) {
     this.x = x;
   }
-  countLoop = (
-    sym: BasicCountType,
-    countWithX: Boolean,
-    ...n: NumParamProps
-  ) => {
+  countLoop = (sym: BasicCountType, ...n: NumParamProps) => {
     let result = 0;
     for (let i = 0; i < n.length; i++) {
       result = result ? eval(`result ${sym} n[i]`) : n[i];
     }
-    this.x = countWithX ? eval(`${this.x}${sym}${result}`) : result;
+    this.x = eval(`${this.x}${sym}${result}`);
     return this.x;
   };
   add(...n: NumParamProps) {
-    return this.countLoop("+", true, ...n);
+    return this.countLoop("+", ...n);
   }
   subtract(...n: NumParamProps) {
-    return this.countLoop("-", true, ...n);
+    return this.countLoop("-", ...n);
   }
-  multiple(withX: Boolean, ...n: NumParamProps) {
-    return this.countLoop("*", withX, ...n);
+  multiple(...n: NumParamProps) {
+    return this.countLoop("*", ...n);
   }
   divide(...n: NumParamProps) {
-    return this.countLoop("/", true, ...n);
+    return this.countLoop("/", ...n);
+  }
+  result() {
+    console.log(this.x);
+    return this.x;
   }
 }
 class ScientCalc extends Calculator {
@@ -50,24 +49,20 @@ class ScientCalc extends Calculator {
   sqrt(n: number) {
     return n * n;
   }
-  pow(props: PowProps) {
-    const powNum = props.powNum || 1;
-    let value = [];
-    for (let i = 0; i < powNum; i++) {
-      value.push(props.n);
+  pow(props: PowProps = { n: 0, x: 1 }) {
+    const x = props.x || 1;
+    const num = props.n || this.x;
+    let result = 1;
+    for (let i = 0; i < x; i++) {
+      result = result * num;
     }
-    return Number(
-      eval(
-        `${this.x}${props.countPreviousNumber}${super.multiple(
-          false,
-          ...value
-        )}`
-      )
-    );
+    this.x = result;
+    return this.x;
   }
   typing(props: { line: string; prev: BasicCountType }) {
     try {
-      return Number(eval(this.x + props.prev + props.line));
+      this.x = Number(eval(this.x + props.prev + props.line));
+      return this.x;
     } catch (error) {
       return "Inputan tidak valid";
     }
@@ -76,7 +71,7 @@ class ScientCalc extends Calculator {
 const getCalculator = new ScientCalc(2);
 console.log("Hasil Tambah =", getCalculator.add(1, 1, 20, 1));
 console.log("Hasil Kurang =", getCalculator.subtract(23));
-console.log("Hasil Kali =", getCalculator.multiple(true, 2, 2));
+console.log("Hasil Kali =", getCalculator.multiple(2, 2));
 console.log("Hasil Bagi =", getCalculator.divide(25, 5));
 console.log(
   "Hasil Ketik =",
@@ -86,7 +81,10 @@ console.log(
   })
 );
 console.log("Hasil pangkat dua =", getCalculator.sqrt(3));
-console.log(
-  "Hasil pangkat =",
-  getCalculator.pow({ n: 2, powNum: 3, countPreviousNumber: "-" })
-);
+console.log("Hasil pangkat =", getCalculator.pow({ x: 2, n: 100 }));
+
+const secondCalc = new ScientCalc(0);
+
+secondCalc.add(3);
+secondCalc.pow({ x: 1 });
+secondCalc.result();
